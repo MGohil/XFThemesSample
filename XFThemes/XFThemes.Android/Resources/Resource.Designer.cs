72dd0d790d0b6e2da22db542bc1056609d40a360
MILAN\MILANGOHIL <milan_213@yahoo.co.in>
2019-02-03 17:28:36 +0530

    Added some Arts and csproj checked in.

--- XFThemes/XFThemes.Android/Resources/Resource.Designer.cs
+++ XFThemes/XFThemes.Android/Resources/Resource.Designer.cs
@@ -2201,26 +2201,26 @@ namespace XFThemes.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f020112
+			public const int avd_hide_password_1 = 2130837778;
+			
 			// aapt resource value: 0x7f020113
-			public const int avd_hide_password_1 = 2130837779;
+			public const int avd_hide_password_2 = 2130837779;
 			
 			// aapt resource value: 0x7f020114
-			public const int avd_hide_password_2 = 2130837780;
-			
-			// aapt resource value: 0x7f020115
-			public const int avd_hide_password_3 = 2130837781;
+			public const int avd_hide_password_3 = 2130837780;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
+			// aapt resource value: 0x7f020115
+			public const int avd_show_password_1 = 2130837781;
+			
 			// aapt resource value: 0x7f020116
-			public const int avd_show_password_1 = 2130837782;
+			public const int avd_show_password_2 = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int avd_show_password_2 = 2130837783;
-			
-			// aapt resource value: 0x7f020118
-			public const int avd_show_password_3 = 2130837784;
+			public const int avd_show_password_3 = 2130837783;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2774,11 +2774,11 @@ namespace XFThemes.Droid
 			// aapt resource value: 0x7f02010c
 			public const int notification_icon_background = 2130837772;
 			
-			// aapt resource value: 0x7f020111
-			public const int notification_template_icon_bg = 2130837777;
+			// aapt resource value: 0x7f020110
+			public const int notification_template_icon_bg = 2130837776;
 			
-			// aapt resource value: 0x7f020112
-			public const int notification_template_icon_low_bg = 2130837778;
+			// aapt resource value: 0x7f020111
+			public const int notification_template_icon_low_bg = 2130837777;
 			
 			// aapt resource value: 0x7f02010d
 			public const int notification_tile_bg = 2130837773;
@@ -2787,10 +2787,7 @@ namespace XFThemes.Droid
 			public const int notify_panel_notification_icon_bg = 2130837774;
 			
 			// aapt resource value: 0x7f02010f
-			public const int profile = 2130837775;
-			
-			// aapt resource value: 0x7f020110
-			public const int theme = 2130837776;
+			public const int theme = 2130837775;
 			
 			static Drawable()
 			{
